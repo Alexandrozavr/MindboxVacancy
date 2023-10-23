57fdac5e94acca1898abb9c833b5f581c4db45e1
Alexandrozavr <55453160+Alexandrozavr@users.noreply.github.com>
2023-10-23 22:09:24 +0300

    Update UnitTest1.cs

--- Q1/Tests/UnitTest1.cs
+++ Q1/Tests/UnitTest1.cs
@@ -5,17 +5,16 @@ namespace Tests
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
         public void Test1()
         {
-            Figure figure = new Triangle(5,4,3);
+            Figure figure = FigureFactory.CreateFigure(new double[] {5,4,3});
             double result = figure.CountSquare();
             Assert.AreEqual(6, result);
         }
         [TestMethod]
         public void Test2()
         {
-            Figure figure = new Circle(5);
+            Figure figure = FigureFactory.CreateFigure(new double[] {5});
             double result = figure.CountSquare();
             Assert.AreEqual(78.5398, result, 0.01);
         }
