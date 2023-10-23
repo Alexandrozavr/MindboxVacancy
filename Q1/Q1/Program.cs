950b6a7ffdda18eafaf60942703c6ac41dc719e3
Alexandrozavr <55453160+Alexandrozavr@users.noreply.github.com>
2023-10-23 21:59:34 +0300

    Add files via upload

---
+++ Q1/Q1/Program.cs
@@ -0,0 +1,44 @@
+﻿using FigureLibrary;
+
+namespace Q1
+{    
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            Figure figure = FigureFactory.CreateFigure(InputAndParcing());
+            Console.WriteLine(figure.CountSquare());
+        }
+
+        private static double[] InputAndParcing()
+        {
+            bool sucsess = false;
+            double[] doubles = null;
+            Console.WriteLine("Введите числа единой строкой. между числами ставьте \";\"." + Environment.NewLine +
+                    "Если хотите получить площадь круга - введите одно число, треугольник - три числа.");
+            while (!sucsess)
+            {
+                sucsess = true;
+                Console.Write("Введите числа: ");
+                string[] inputs = Console.ReadLine().Split(';');
+                if (inputs.Length != 1 && inputs.Length != 3)
+                {
+                    sucsess = false;
+                    Console.WriteLine("Неверное колличество введеных чисел!");
+                    continue;
+                }
+                doubles = new double[inputs.Length];
+                for (int i = 0; i < inputs.Length; i++)
+                {
+                    sucsess = double.TryParse(inputs[i], out doubles[i]);
+                    if (!sucsess)
+                    {
+                        Console.WriteLine($"Неверный ввод {i + 1}-го числа");
+                        break;
+                    }                                        
+                }
+            }
+            return doubles;
+        }
+    }
+}
---
+++ Q1/Tests/UnitTest1.cs
@@ -0,0 +1,39 @@
+using FigureLibrary;
+
+namespace Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Test1()
+        {
+            Figure figure = new Triangle(5,4,3);
+            double result = figure.CountSquare();
+            Assert.AreEqual(6, result);
+        }
+        [TestMethod]
+        public void Test2()
+        {
+            Figure figure = new Circle(5);
+            double result = figure.CountSquare();
+            Assert.AreEqual(78.5398, result, 0.01);
+        }
+        [TestMethod]
+        public void Test3()
+        {
+            Triangle figure = new Triangle(6, 4, 3);
+         
+            bool result = figure.IsSquareAngeled();
+            Assert.AreEqual(false, result);
+        }
+        [TestMethod]
+        public void Test4()
+        {
+            Triangle figure = new Triangle(5, 4, 3);
+         
+            bool result = figure.IsSquareAngeled();
+            Assert.AreEqual(true, result);
+        }
+    }
+}
