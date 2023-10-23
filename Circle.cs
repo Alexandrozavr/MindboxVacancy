6f016158bae7fe5c3760ae510f89e878329cab1e
Alexandrozavr <55453160+Alexandrozavr@users.noreply.github.com>
2023-10-23 21:54:56 +0300

    Add files via upload

---
+++ Circle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FigureLibrary
+{
+    public class Circle : Figure
+    {
+        private double r;
+        public Circle(double r)
+        {
+            this.r = r;
+        }
+
+        public override double CountSquare()
+        {
+            return Math.PI * r * r;
+        }
+    }
+}
